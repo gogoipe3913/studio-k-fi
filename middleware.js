fce08906efdf810341a94d9a9cd818ac12eb2f19
Taiki Kishiyama <taiki.kishiyama0913@gmail.com>
2024-08-16 14:14:28 +0900

    basic認証をかける

--- middleware.js
+++ middleware.js
@@ -1,21 +1,28 @@
-import { next } from "@vercel/edge";
-
 export const config = {
-  matcher: ["/", "/:path*"],
+  matcher: ["/(.*)"], // すべてのパスに対してミドルウェアを適用
 };
 
 export default function middleware(request) {
   const authorizationHeader = request.headers.get("authorization");
 
-  if (authorizationHeader) {
-    const basicAuth = authorizationHeader.split(" ")[1];
-    const [user, password] = atob(basicAuth).toString().split(":");
+  // 環境変数からユーザー名とパスワードを取得
+  const username = process.env.USERNAME;
+  const password = process.env.PASSWORD;
 
-    if (user === process.env.USERNAME && password === process.env.PASSWORD) {
-      return next();
+  if (authorizationHeader) {
+    const [scheme, encodedCredentials] = authorizationHeader.split(" ");
+    if (scheme === "Basic") {
+      const [providedUsername, providedPassword] =
+        atob(encodedCredentials).split(":");
+
+      // 認証チェック
+      if (providedUsername === username && providedPassword === password) {
+        return new Response(null, { status: 200 });
+      }
     }
   }
 
+  // 認証が失敗した場合、401エラーを返す
   return new Response("Basic Auth required", {
     status: 401,
     headers: {
